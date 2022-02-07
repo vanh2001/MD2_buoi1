726e7fc62cb0ef1de9d57f47a164f7664ace3bd6
vanh2001 <anhviet1709@gmail.com>
2022-02-07 14:34:57 +0700

    Add bai 2 buoi 2

---
+++ variable-dataType/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10; // khai báo biến i là kiểu int có giá trị là 10
+        System.out.println("i = " + i);
+        float f = 20.5f; // khai báo biến f là kiểu float có giá trị là 20.5
+        System.out.println("f = " + f);
+        double d = 20.5; // khai báo biến d là kiểu double có giá trị là 20.5
+        System.out.println("d = " + d);
+        boolean b = true; // khai báo biến b là kiểu boolean có giá trị là true
+        System.out.println("b = " + b);
+        char c = 'c'; // khai báo biến c là kiểu char có giá trị là ký tự c
+        System.out.println("c = " + c);
+        String s = "Hà Nội"; // khai báo biến i là kiểu String có giá trị là chuỗi Hà Nội
+        System.out.println("s = " + s);
+    }
+}
